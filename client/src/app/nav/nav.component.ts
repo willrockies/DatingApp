92fafb31de284ca8c97bcd21b0b846edbc4ef6c2
willrockies <wlsn.ads@gmail.com>
2022-05-05 11:45:31 -0300

    client login

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -9,22 +9,32 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
 
   model: any = {}
-
+  loggedIn!: boolean;
   constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.getCurrentUser();
   }
 
   login() {
 
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
+      this.loggedIn = true;
     }, error => {
       console.log(error);
     });;
   }
   logout() {
     this.accountService.logout();
+    this.loggedIn = false;
   }
-  
+
+  getCurrentUser() {
+    this.accountService.currentUser$.subscribe(user => {
+      this.loggedIn = !!user;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
